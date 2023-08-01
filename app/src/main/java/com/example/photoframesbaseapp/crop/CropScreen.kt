4070a96a6959abf46884f6bea3a21c752733012b
Lenovo <vallemkishore@gmail.com>
2023-08-01 16:08:28 -0400

    added the image crop functionality.

--- app/src/main/java/com/example/photoframesbaseapp/crop/CropScreen.kt
+++ app/src/main/java/com/example/photoframesbaseapp/crop/CropScreen.kt
@@ -1,9 +1,128 @@
 package com.example.photoframesbaseapp.crop
 
-import androidx.compose.runtime.Composable
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
+import coil.compose.rememberImagePainter
+import com.example.photoframesbaseapp.BuildConfig
+import com.example.photoframesbaseapp.share.findActivity
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 @Composable
 fun CropScreen(navHostController: NavHostController) {
 
+}
+
+@Composable
+fun AppContent() {
+
+    val context = LocalContext.current
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        BuildConfig.APPLICATION_ID + ".provider", file
+    )
+
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+
+    val cameraLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+            capturedImageUri = uri
+
+            try {
+                val cropIntent = Intent("com.android.camera.action.CROP")
+                cropIntent.setDataAndType(capturedImageUri, "image/*")
+                cropIntent.putExtra("crop", true)
+                cropIntent.putExtra("aspectX", 1)
+                cropIntent.putExtra("aspectY", 1)
+                cropIntent.putExtra("outputX", 128)
+                cropIntent.putExtra("outputY", 128)
+                cropIntent.putExtra("return-data", true)
+                startActivityForResult(context.findActivity()!!,cropIntent, 100, null)
+            } // respond to users whose devices do not support the crop action
+            catch (anfe: ActivityNotFoundException) {
+                // display an error message
+                val errorMessage = "Whoops - your device doesn't support the crop action!"
+                val toast = Toast.makeText(context.findActivity()!!, errorMessage, Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            val permissionCheckResult =
+                ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+            if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                cameraLauncher.launch(uri)
+            } else {
+                // Request a permission
+                permissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }) {
+            Text(text = "Capture Image From Camera")
+        }
+    }
+
+    if (capturedImageUri.path?.isNotEmpty() == true) {
+        Image(
+            modifier = Modifier
+                .padding(16.dp, 8.dp),
+            painter = rememberImagePainter(capturedImageUri),
+            contentDescription = null
+        )
+    }
+}
+
+fun Context.createImageFile(): File {
+    // Create an image file name
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val imageFileName = "JPEG_" + timeStamp + "_"
+    val image = File.createTempFile(
+        imageFileName, /* prefix */
+        ".jpg", /* suffix */
+        externalCacheDir      /* directory */
+    )
+    return image
 }
